db52f1f4bab0d598efc7cbbf387745a2ff3a2ef5
T. Betten <tbetten@hotmail.com>
2020-03-11 08:40:35 +0100

    Add project files.

---
+++ db/db.h
@@ -0,0 +1,97 @@
+#pragma once  
+#include <memory>
+#include <string>
+#include <exception>
+#include <stdexcept>
+#include <unordered_map>
+#include <vector>
+#include <variant>
+
+struct sqlite3_stmt;
+struct sqlite3;
+
+namespace db
+{
+	using namespace std::string_literals;
+	struct Blob
+	{
+		Blob() : size{ 0 }, value{ nullptr }{}
+		Blob(const Blob& other) noexcept;
+		Blob& operator=(const Blob& other) noexcept;
+		Blob(Blob&& other) noexcept;
+		Blob& operator=(Blob&& other) noexcept;
+		~Blob() noexcept;
+
+		size_t size;
+		void* value;
+	};
+
+	class Prepared_statement;
+	class DB_connection
+	{
+	public:
+		enum class Mode { Read_only, Read_write, Create };
+
+		DB_connection() : m_path{ ""s }, m_connection{ nullptr }, m_mode{ Mode::Read_only } {}
+		explicit DB_connection(std::string path, Mode mode = Mode::Read_only);
+		DB_connection(const DB_connection& other);
+		DB_connection& operator= (DB_connection other);
+		DB_connection(DB_connection&& other) noexcept;
+		DB_connection& operator= (DB_connection&& other) noexcept;
+		~DB_connection() noexcept;
+		friend void swap(DB_connection& first, DB_connection& second) noexcept
+		{
+			using std::swap;
+			swap(first.m_path, second.m_path);
+			swap(first.m_mode, second.m_mode);
+			swap(first.m_connection, second.m_connection);
+		}
+		Prepared_statement prepare(std::string sql);
+	private:
+		void close() noexcept;
+
+		std::string m_path;
+		Mode m_mode;
+		sqlite3* m_connection;
+	};
+
+	using value_t = std::variant<std::monostate, int, double, std::string, Blob>;
+	using row_t = std::unordered_map<std::string, value_t>;
+	using table_t = std::vector<row_t>;
+
+	class Prepared_statement
+	{
+	public:
+		enum class Row_result { Success, Row, Error };
+
+		Prepared_statement() : m_statement{ nullptr } {}
+		explicit Prepared_statement(sqlite3_stmt* stmt) : m_statement{ stmt } {}
+		Prepared_statement(const Prepared_statement& other) = delete;
+		Prepared_statement& operator=(Prepared_statement& other) = delete;
+		Prepared_statement(Prepared_statement&& other) noexcept;
+		Prepared_statement& operator=(Prepared_statement&& other) noexcept;
+		~Prepared_statement() noexcept;
+
+		void bind(int index, value_t value);
+		Row_result execute_row();
+		row_t fetch_row();
+		table_t fetch_table();
+		void reset() noexcept;
+	private:
+		sqlite3_stmt* m_statement;
+	};
+
+	class db_connection;
+	class prepared_statement;
+	struct resultset;
+
+	using db_connection_ptr = std::unique_ptr<db_connection>;
+	using prepared_statement_ptr = std::shared_ptr<prepared_statement>;
+	//using value_t = std::variant<std::monostate, int, double, std::string>;
+
+	class db_exception : public std::runtime_error
+	{
+		using std::runtime_error::runtime_error;
+	};
+
+}
---
+++ db_tester/db_tester.cpp
@@ -0,0 +1,47 @@
+#include "db.h"
+#include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+/*
+C:\Users\T. Betten\source\repos\db\db_tester>"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.24.28314\bin\HostX86\x86"\link.exe /ERRORREPORT:PROMPT /OUT:"C:\Users\T. Betten\source\repos\db\Debug\db_tester.exe" /INCREMENTAL /NOLOGO /LIBPATH:"C:\Users\T. Betten\source\repos\db\Debug" /LIBPATH:"D:\vcpkg\installed\x86-windows\debug\lib" /LIBPATH:"D:\vcpkg\installed\x86-windows\debug\lib\manual-link" db.lib kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib "D:\vcpkg\installed\x86-windows\debug\lib\*.lib" /MANIFEST /MANIFESTUAC:"level='asInvoker' uiAccess='false'" /manifest:embed /DEBUG:FASTLINK /PDB:"C:\Users\T. Betten\source\repos\db\Debug\db_tester.pdb" /SUBSYSTEM:CONSOLE /TLBID:1 /DYNAMICBASE /NXCOMPAT /IMPLIB:"C:\Users\T. Betten\source\repos\db\Debug\db_tester.lib" /MACHINE:X86 Debug\db_tester.obj
+db.lib(db.obj) : error LNK2019: unresolved external symbol _sqlite3_close_v2 referenced in function "public: __thiscall db::DB_connection::DB_connection(class std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> >,enum db::DB_connection::Mode)" (??0DB_connection@db@@QAE@V?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@W4Mode@01@@Z)
+*/
+
+
+int main()
+{
+	db::Blob b{};
+	b.value = std::malloc(4);
+	if (b.value) std::memset(b.value, 'A', 4);
+	db::Blob c{};
+	c.value = std::malloc(5);
+	if (c.value) std::memset(c.value, 'B', 5);
+	c.size = 5;
+
+	b.size = 4;
+	db::Blob b1{ b };
+	if (!b.value) return 0;
+	std::string s{ static_cast<char*> (b.value), 4 };
+	std::cout << std::string{ static_cast<char*> (b.value), b.size } << "\n";
+	std::cout << std::string{ static_cast<char*> (b1.value), b1.size } << "\n";
+	b1 = c;
+	std::cout << std::string{ static_cast<char*> (b1.value), b1.size } << "\n";
+	db::Blob b2{ std::move(b1) };
+	db::Blob b3{};
+	b3 = std::move(b2);
+	std::cout << std::string{ static_cast<char*> (b3.value), b3.size } << "\n";
+
+	std::cout << fs::current_path() << "\n";
+	db::DB_connection db{ "db1.db" };
+
+	auto stmt = db.prepare("select desc from test where code = ?");
+	stmt.bind(1, 2);
+	auto table = stmt.fetch_table();
+	for (auto row : table)
+	{
+		auto val = std::get<std::string> (row["desc"]);
+		std::cout << val << "\n";
+	}
+}
